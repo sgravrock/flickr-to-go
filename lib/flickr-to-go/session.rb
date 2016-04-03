c5f4d68912d7910562f867bb56bc6ea138b5185d
Steve Gravrock <sdg@panix.com>
2016-04-03 09:08:06 -0700

    Moved flickraw initialization into Session

--- lib/flickr-to-go/session.rb
+++ lib/flickr-to-go/session.rb
@@ -1,5 +1,13 @@
+require 'flickraw'
+
 module FlickrToGo
 	class Session
+		def self.with_app_credentials(api_key, secret)
+			FlickRaw.api_key = api_key
+			FlickRaw.shared_secret = secret
+			Session.new(flickr, ConsoleAuthUiAdapter.new)
+		end
+
 		def initialize(flickr_client, auth_ui_adapter)
 			@flickr = flickr_client
 			@auth_ui_adapter = auth_ui_adapter
@@ -13,24 +21,21 @@ module FlickrToGo
 		end
 
 		attr_accessor(:access_token, :access_secret)
+		attr_reader(:flickr)
 
 		private
 
 		def get_access_code(oauth_token)
-			url = @flickr.get_authorize_url(oauth_token['oauth_token'])
+			url = flickr.get_authorize_url(oauth_token['oauth_token'])
 			code = @auth_ui_adapter.request_access_code(url)
 		end
 
 		def redeem_access_code(oauth_token, access_code)
-			@flickr.get_access_token(oauth_token['oauth_token'],
+			flickr.get_access_token(oauth_token['oauth_token'],
 				oauth_token['oauth_token_secret'], access_code)
-			self.access_token = @flickr.access_token
-			self.access_secret = @flickr.access_secret
+			self.access_token = flickr.access_token
+			self.access_secret = flickr.access_secret
 			true
-		end
-
-		def flickr
-			raise 'Use @flickr, not the global flickr variable'
 		end
 	end
 end
