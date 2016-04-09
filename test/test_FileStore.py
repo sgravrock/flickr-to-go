084bf3f8665db06f543bc8b769e366b1cae93379
Steve Gravrock <sdg@panix.com>
2016-04-08 21:28:23 -0700

    Fixed directory leak in unit tests

--- test/test_FileStore.py
+++ test/test_FileStore.py
@@ -8,20 +8,21 @@ import storage
 def raise_io_error(*args):
     raise IOError('nope!')
 
+@patch('os.mkdir')
 @patch('storage.open')
 class TestFileStore(unittest.TestCase):
-    def test_save_json(self, mock_open):
+    def test_save_json(self, mock_open, mock_mkdir):
         subject = storage.FileStore('root')
         subject.save_json('foo', {})
         mock_open.assert_called_with('root/foo.json', 'w')
 
-    def test_save_image(self, mock_open):
+    def test_save_image(self, mock_open, mock_mkdir):
         subject = storage.FileStore('root')
         subject.save_image('foo.jpg', '')
         mock_open.assert_called_with('root/foo.jpg', 'wb')
 
     @patch('os.path.exists')
-    def test_exists(self, mock_exists, mock_open):
+    def test_exists(self, mock_exists, mock_open, mock_mkdir):
         mock_exists.return_value = True
         subject = storage.FileStore('root')
         self.assertTrue(subject.exists('foo'))
@@ -29,24 +30,23 @@ class TestFileStore(unittest.TestCase):
         mock_exists.return_value = False
         self.assertFalse(subject.exists('foo'))
 
-    def test_credentials_path(self, mock_open):
+    def test_credentials_path(self, mock_open, mock_mkdir):
         subject = storage.FileStore('root')
         self.assertEqual('root/flickr-credentials',
                 subject.credentials_path())
 
-    def test_has_saved_credentials_true(self, mock_open):
+    def test_has_saved_credentials_true(self, mock_open, mock_mkdir):
         subject = storage.FileStore('root')
         self.assertTrue(subject.has_saved_credentials());
         mock_open.assert_called_with('root/flickr-credentials')
 
-    def test_has_saved_credentials_false(self, mock_open):
+    def test_has_saved_credentials_false(self, mock_open, mock_mkdir):
         mock_open.side_effect = raise_io_error
         subject = storage.FileStore('root')
         self.assertFalse(subject.has_saved_credentials());
         mock_open.assert_called_with('root/flickr-credentials')
 
-    @patch('os.mkdir')
-    def test_ensure_dir(self, mock_mkdir, mock_open):
+    def test_ensure_dir(self, mock_open, mock_mkdir):
         subject = storage.FileStore('root')
         subject.ensure_dir('foo')
         mock_mkdir.ensure_called_with('root/foo')
