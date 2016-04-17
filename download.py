08065faa75c70cf32fe235a3f38880a5491870e8
Steve Gravrock <sdg@panix.com>
2016-04-17 16:41:16 -0700

    Removed duplicate fetch code

--- download.py
+++ download.py
@@ -13,17 +13,10 @@ class FlickrApiDownloader:
 
     def download(self, method, params, data_accessor, dest_path):
         params = _combine(params, {'format': 'json', 'nojsoncallback': 1})
-        try:
-            doc = method(**params)
-        except HTTPException, e:
-            self.error_handler.add_error(method, params, e)
-            return None
-        parsed = json.loads(doc)
-        if not _standard_test_response(parsed):
-            self.error_handler.add_error(method, params, parsed)
-            return None
-        result = data_accessor(parsed)
-        self.file_store.save_json(dest_path, result)
+        result = self._fetch(method, params, data_accessor,
+                _standard_test_response)
+        if result:
+            self.file_store.save_json(dest_path, result)
         return result
 
     def paged_download(self, method, params, data_accessor, page_size,
@@ -37,16 +30,7 @@ class FlickrApiDownloader:
             page_ix, test_response=_standard_test_response):
         params = _combine(extra_params, {'page': page_ix,
                 'per_page': page_size, 'format': 'json', 'nojsoncallback': 1})
-        try:
-            doc = method(**params)
-        except HTTPException, e:
-            self.error_handler.add_error(method, params, e)
-            return None
-        parsed = json.loads(doc)
-        if not test_response(parsed):
-            self.error_handler.add_error(method, params, parsed)
-            return None
-        return data_accessor(parsed)
+        return self._fetch(method, params, data_accessor, test_response)
 
     def _paged_fetch(self, method, extra_params, data_accessor, page_size):
         page_ix = 1
@@ -60,6 +44,18 @@ class FlickrApiDownloader:
             if len(page) < page_size:
                 return result
             page_ix += 1
+
+    def _fetch(self, method, params, data_accessor, test_response):
+        try:
+            doc = method(**params)
+        except HTTPException, e:
+            self.error_handler.add_error(method, params, e)
+            return None
+        parsed = json.loads(doc)
+        if not test_response(parsed):
+            self.error_handler.add_error(method, params, parsed)
+            return None
+        return data_accessor(parsed)
 
 
 
