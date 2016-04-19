e7adaa7c4934f97233a38b9c022858c72867f527
Steve Gravrock <sgravrock@pivotal.io>
2016-04-19 11:17:32 -0700

    Retry up to twice when a photo download fails

--- photo.py
+++ photo.py
@@ -10,11 +10,20 @@ def download(photos, downloader, flickr):
 def download_originals(photolist, file_store, requests=requests,
         logger=sys.stdout):
     for photo in photolist:
-        filename = original_filename(photo)
-        if not file_store.exists(filename):
-            logger.write("Downloading %s\n" % photo['id'])
-            response = requests.get(photo['url_o'])
-            file_store.save_image(filename, response.content)
+        _download_original(photo, file_store, requests, logger)
+
+def _download_original(photo, file_store, requests, logger):
+    filename = original_filename(photo)
+    if not file_store.exists(filename):
+        logger.write("Downloading %s\n" % photo['id'])
+        for i in xrange(0, 3):
+            try:
+                response = requests.get(photo['url_o'])
+            except Exception, e:
+                if i == 2:
+                    raise
+        file_store.save_image(filename, response.content)
+        return
 
 def original_filename(photo):
     return 'originals/%s_o.jpg' % photo['id']
