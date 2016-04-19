dc8b65d721aa302c141de3576f7ddd7d381fd0d3
Steve Gravrock <sgravrock@pivotal.io>
2016-04-19 11:17:32 -0700

    wip

--- test/test_photo.py
+++ test/test_photo.py
@@ -3,7 +3,42 @@ from mock import Mock, call
 import photo
 import json
 from StringIO import StringIO
+import requests
 from download import FlickrApiDownloader
+
+class ThrowsTwice:
+    def __init__(self, successful_response):
+        self.successful_response = successful_response
+        self.count = 0
+
+    def get(self, url):
+        self.count += 1
+        if self.count == 3:
+            resp = requests.models.Response()
+            resp.status_code = 200
+            resp.content = self.successful_response
+            return resp
+        else:
+            raise requests.exceptions.ConnectionError('nope')
+
+
+class ErrorsTwice:
+    def __init__(self, successful_response):
+        self.successful_response = successful_response
+        self.count = 0
+
+    def get(self, url):
+        self.count += 1
+        resp = requests.models.Response()
+        if self.count == 3:
+            resp.status_code = 200
+            resp.content = self.successful_response
+        else:
+            resp.status_code = 500
+        return resp
+
+class UnmockedUrlException(Exception):
+    pass
 
 class MockRequests:
     def __init__(self):
@@ -11,7 +46,7 @@ class MockRequests:
 
     def get(self, url):
         if url not in self.contents:
-            raise Exception('Un-mocked URL: ' + url)
+            raise UnmockedUrlException('Un-mocked URL: ' + url)
         return MockResponse(self.contents[url])
 
 class MockResponse:
@@ -50,6 +85,40 @@ class TestPhoto(unittest.TestCase):
         file_store.save_image.assert_has_calls([
                 call('originals/25461030990_o.jpg', responses[0]),
                 call('originals/23793491473_o.jpg', responses[1])])
+
+    def test_download_originals_exception_retries(self):
+        photos = [{'id': '25461030990', 'url_o': 'https://farm2.staticflickr.com/1521/25461030990_3621f6ae2d_o.jpg'}]
+        response = '\xff\xd8\xff\xe1\x16&Exif\x00\x00II*\x00\x08\x00\x00\x00'
+        requests = ThrowsTwice(response)
+        file_store = Mock()
+        file_store.exists.return_value = False
+        photo.download_originals(photos, file_store, requests, StringIO())
+        file_store.save_image.assert_has_calls([
+                call('originals/25461030990_o.jpg', response)])
+
+    def test_download_originals_bad_status_retries(self):
+        photos = [{'id': '25461030990', 'url_o': 'https://farm2.staticflickr.com/1521/25461030990_3621f6ae2d_o.jpg'}]
+        response = '\xff\xd8\xff\xe1\x16&Exif\x00\x00II*\x00\x08\x00\x00\x00'
+        requests = ErrorsTwice(response)
+        file_store = Mock()
+        file_store.exists.return_value = False
+        photo.download_originals(photos, file_store, requests, StringIO())
+        file_store.save_image.assert_has_calls([
+                call('originals/25461030990_o.jpg', response)])
+
+    def test_download_originals_eventually_fails(self):
+        photos = [{'id': '25461030990', 'url_o': 'https://farm2.staticflickr.com/1521/25461030990_3621f6ae2d_o.jpg'}]
+        requests = MockRequests() # And don't provide a response
+        file_store = Mock()
+        file_store.exists.return_value = False
+        threw = False
+        try:
+            photo.download_originals(photos, file_store, requests,
+                    StringIO())
+        except UnmockedUrlException:
+            threw = True
+        self.assertTrue(threw)
+        file_store.save_image.assert_not_called()
 
     def test_download_originals_skips_existing(self):
         photos = [{'id': '25461030990', 'url_o': 'https://farm2.staticflickr.com/1521/25461030990_3621f6ae2d_o.jpg'}]
