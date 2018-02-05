84ee6bdc62ba8b58cd889caeedfd737e230464b1
Steve Gravrock <sdg@panix.com>
2018-02-04 22:24:09 -0800

    Removed remaining use of @patch from storage tests

--- test_FileStore.py
+++ test_FileStore.py
@@ -5,12 +5,7 @@ import tempfile
 import shutil
 import os
 from contextlib import contextmanager
-from mock import patch
-from StringIO import StringIO
 import storage
-
-def raise_io_error(*args):
-    raise IOError('nope!')
 
 @contextmanager
 def in_temp_dir():
@@ -32,31 +27,28 @@ def pushd(dest):
          os.chdir(src)
 
 class TestFileStore(unittest.TestCase):
-    @patch('os.mkdir')
-    @patch('storage.open')
-    def test_save_json(self, mock_open, mock_mkdir):
-        subject = storage.FileStore('root')
-        subject.save_json('foo/bar.json', {})
-        mock_mkdir.assert_called_with('root/foo')
-        mock_open.assert_called_with('root/foo/bar.json', 'w')
+    def test_save_json(self):
+        with in_temp_dir():
+            os.mkdir('root')
+            subject = storage.FileStore('root')
+            subject.save_json('foo/bar.json', {})
+            with open('root/foo/bar.json') as f:
+                self.assertEqual({}, json.load(f))
 
-    @patch('os.mkdir')
-    @patch('storage.open')
-    def test_save_image(self, mock_open, mock_mkdir):
-        subject = storage.FileStore('root')
-        subject.save_image('foo.jpg', '')
-        mock_open.assert_called_with('root/foo.jpg', 'wb')
+    def test_save_image(self):
+        with in_temp_dir():
+            subject = storage.FileStore('root')
+            subject.save_image('foo.jpg', '')
+            self.assertTrue(os.path.exists('root/foo.jpg'))
 
-    @patch('os.mkdir')
-    @patch('storage.open')
-    @patch('os.path.exists')
-    def test_exists(self, mock_exists, mock_open, mock_mkdir):
-        mock_exists.return_value = True
-        subject = storage.FileStore('root')
-        self.assertTrue(subject.exists('foo'))
-        mock_exists.assert_called_with('root/foo')
-        mock_exists.return_value = False
-        self.assertFalse(subject.exists('foo'))
+    def test_exists(self):
+        with in_temp_dir():
+            subject = storage.FileStore('root')
+            os.mkdir('root')
+            self.assertFalse(subject.exists('foo'))
+            with open('root/foo', 'w'):
+                pass
+            self.assertTrue(subject.exists('foo'))
 
     def test_saved_credentials_some(self):
         with in_temp_dir():
